25abc00a5e7cdf1b2ec075740dd6cdceed5548a6
Levyslater <lawre@Xshader>
2025-07-28 16:24:53 +0300

    use login and logout builtin views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views as user_views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', user_views.list_books, name='all_books'),
     path('library/<int:pk>/', user_views.LibraryDetailView.as_view(), name='library_detail'),
     path('register/', user_views.register_user, name='register'),
-    path('login/', user_views.user_login, name='login'),
-    path('logout/', user_views.user_logout, name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
