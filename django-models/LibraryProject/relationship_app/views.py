0134fd9ce2bf1136f92bf188110aa5763f47ba6d
Levyslater <lawre@Xshader>
2025-07-28 16:08:56 +0300

    import modules individually

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render,redirect
 from django.views.generic.detail import DetailView
 from .models import Library, Book
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import logout
+
 
 
 def list_books(request):
