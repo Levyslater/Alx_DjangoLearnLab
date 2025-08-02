46b328e70c730f5aa8e7d0e92d791bb9773593fa
Levyslater <lawre@Xshader.localdomain>
2025-08-02 12:42:45 +0300

    url should be 'edit_book' not 'change_book'

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('librarian/', user_views.librarian_view, name='librarian_view'),
     path('member/', user_views.member_view, name='member_view'),
     path('add_book/', user_views.add_book, name='add_book'),
-    path('change_book/<int:book_id>/', user_views.change_book, name='change_book'),
+    path('edit_book/<int:book_id>/', user_views.change_book, name='change_book'),
     path('delete_book/<int:book_id>/', user_views.delete_book, name='delete_book'),
 
 ]
