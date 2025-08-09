eaff73a9fa9b465a624d8260012c5e9db01bb943
Levyslater <lawre@Xshader.localdomain>
2025-08-09 13:15:31 +0300

    Using ViewSets and Routers to minimize boilerplate code for CRUD operations and URL structure

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 from .models import Book
 from rest_framework import generics
 from .serializers import BookSerializer
+from rest_framework import viewsets
 
 # Create your views here.
-class BookList(generics.ListAPIView):
-    serializer_class = BookSerializer
+class BooklistView(generics.ListAPIView):
+    """
+    A view to list all books.
+    """
     queryset = Book.objects.all()
+    serializer_class = BookSerializer
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing book instances.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
