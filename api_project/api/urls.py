b6510e064d939ede8003f04289651d189367ecca
Levyslater <lawre@Xshader.localdomain>
2025-08-09 17:41:31 +0300

    Using token authentication and permissions

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from .views import BookViewSet
 from rest_framework.routers import DefaultRouter
 from .views import BooklistView
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -9,4 +11,5 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BooklistView.as_view(), name='book-list'),
     path('', include(router.urls)),
+    path('token/', obtain_auth_token),
 ]
